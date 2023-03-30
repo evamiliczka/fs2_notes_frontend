6662817e09ac23563b1fa2c99b512a344a55ad12
eva miliczka <eva.miliczka@gmail.com>
2023-03-30 10:08:44 +0200

    Exercises fully working, styles imporved

--- src/App_FullStack2.js
+++ src/App_FullStack2.js
@@ -12,11 +12,11 @@ const heading = {
         <div>
             <h1> Helsinky Full Stack Course Part 2</h1>
             <p  style={{fontStyle: "italic"}}> "Communicating with the server" </p>
-            <h3 style={heading}> 1. Exercises: </h3>
+            <h3 style={heading}> 1. Exercises <a href="https://fullstackopen.com/en/part2"> 2.1 to 2.17  </a> </h3>
             <Phonebook />
             <br></br>
             <br></br>
-            <h3 style={heading}> 2. Course content: </h3>
+            <h3 style={heading}> 2. Course <a href="https://fullstackopen.com/en/part2">content </a> </h3>
             <Notes />
         </div>
     )
