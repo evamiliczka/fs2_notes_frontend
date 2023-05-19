e4bc48527760a93239d4c40815223d57839e561d
evamiliczka <109716067+evamiliczka@users.noreply.github.com>
2023-05-19 11:17:34 +0200

    Delete index_2b_Ex_phoneBook.js

--- src/index_2b_Ex_phoneBook.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import PhoneBook from './Ex2FullStack2b_phoneBook'
-
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <PhoneBook />
-)
