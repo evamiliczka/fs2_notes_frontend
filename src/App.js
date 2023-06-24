be3da463ca123a4c0ea9da42a379e73aab3d3aba
eva miliczka <eva.miliczka@gmail.com>
2023-06-24 22:42:55 +0200

    Handling login on frontend

--- src/App.js
+++ src/App.js
@@ -1,92 +1,158 @@
-import { React,useState, useEffect } from 'react'
-import Note from './components/Note'
-import noteService from './services/notes'
-import Notification from './components/Notification'
-import './notes.css'
+import { React, useState, useEffect } from 'react';
+import Note from './components/Note';
+import noteService from './services/notes';
+import loginService from './services/login';
+import Notification from './components/Notification';
+import './notes.css';
+
+
 
 
 
 const App = () => {
-  const [notes, setNotes] = useState([])
-  const [newNote, setNewNote] = useState('')
-  const [showAll, setShowAll] = useState(false)
-  const [errorMessage, setErrorMessage] = useState(null)  
+  const [notes, setNotes] = useState([]);
+  const [newNote, setNewNote] = useState('');
+  const [showAll, setShowAll] = useState(false);
+  const [errorMessage, setErrorMessage] = useState(null);
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [user, setUser] = useState(null);
 
-useEffect(() => {
-  noteService.getAll()
-    .then(allNotes => {
-        setNotes(allNotes)
-  })
-}, [])
+  const loginForm = () => (
+    <form onSubmit={handleLogin}>
+          <div>
+            username
+            <input
+              type='text'
+              value={username}
+              onChange={({ target }) => setUsername(target.value)}
+            />
+          </div>
+          <div>
+            password
+            <input
+              type='text'
+              value={password}
+              onChange={({ target }) => setPassword(target.value)}
+            />
+          </div>
+          <button type='submit'>login</button>
+        </form>
+  )
   
-//console.log('render', notes.length, 'notes')
-
-const addNote = (event) => {
-  event.preventDefault()
-  const noteObject = {
-    content: newNote,
-    important: Math.random() > 0.5,
-  }
-
-  noteService
-    .create(noteObject)
-    .then(returnedNote => {
-      setNotes(notes.concat(returnedNote));
-      setNewNote('')
-    })   
-}
-
-const toggleImportanceOf = (id) => {
-  const note = notes.find(n => n.id === id);
-  const changedNote = {...note, important: !note.important}; //!! shallow copy
-  noteService
-    .update(id, changedNote)
-    .then(returnedNote => {
-      setNotes(notes.map(n => n.id !== id ? n : returnedNote )) //tu sem zosynchronizovali nacitane data
-    })
-    //catch is called if any of the promises in the chain throws an error
-    .catch(error => {
-      setErrorMessage(`Note '${note.content}' was already removed from server`);
-      setTimeout(() => {
-        setErrorMessage(null)
-      }, 5000);
-      setNotes(notes.filter(n => n.id !== id)); //zobrazim len tie poznamky, ktore maju ine id ako ta bludna
-    })
-
-}
-
-const handleNoteChange = (event) => {
-  setNewNote(event.target.value)
-}
-
-const notesToShow = showAll
-  ? notes
-  : notes.filter(note => note.important)
-
-return (
-  <div>
-    <h1>Notes</h1>
-    <Notification message={errorMessage} />
-
-    <div>
+  const notesDisplayed = () => (
+     <>
+      <div>
       <button onClick={() => setShowAll(!showAll)}>
-        show {showAll ? 'important' : 'all' }
+        show {showAll ? 'important' : 'all'}
       </button>
-    </div> 
+    </div>
     <ul>
       <ul>
-        {notesToShow.map(note => 
-          <Note key={note.id} note={note} toggleImportance={() => toggleImportanceOf(note.id)} />
-        )}
+        {notesToShow.map((note) => (
+          <Note
+            key={note.id}
+            note={note}
+            toggleImportance={() => toggleImportanceOf(note.id)}
+          />
+        ))}
       </ul>
     </ul>
+    </>
+  )
+
+
+  const notesForm = () => 
+   ( <>
+   
     <form onSubmit={addNote}>
       <input value={newNote} onChange={handleNoteChange} />
-      <button type="submit">save</button>
+      <button type='submit'>save</button>
     </form>
-   
-  </div>
-)
-}
+  </>)
+  
 
-export default App
+
+  const handleLogin = async (event) => {
+    event.preventDefault();
+    try {
+      const user = await loginService.login({ username, password });
+      console.log(user);
+      setUser(user);
+      setUsername('');
+      setPassword('');
+    } catch (exception){
+      setErrorMessage('Wrong username or password');
+      setTimeout(() => {
+        setErrorMessage(null)
+      }, 5000)
+    }
+  };
+
+
+  useEffect(() => {
+    noteService.getAll().then((allNotes) => {
+      setNotes(allNotes);
+    });
+  }, []);
+
+  //console.log('render', notes.length, 'notes')
+
+  const addNote = (event) => {
+    event.preventDefault();
+    const noteObject = {
+      content: newNote,
+      important: Math.random() > 0.5,
+    };
+
+    noteService.create(noteObject).then((returnedNote) => {
+      setNotes(notes.concat(returnedNote));
+      setNewNote('');
+    });
+  };
+
+  const toggleImportanceOf = (id) => {
+    const note = notes.find((n) => n.id === id);
+    const changedNote = { ...note, important: !note.important }; //!! shallow copy
+    noteService
+      .update(id, changedNote)
+      .then((returnedNote) => {
+        setNotes(notes.map((n) => (n.id !== id ? n : returnedNote))); //tu sem zosynchronizovali nacitane data
+      })
+      //catch is called if any of the promises in the chain throws an error
+      .catch((error) => {
+        setErrorMessage(
+          `Note '${note.content}' was already removed from server`
+        );
+        setTimeout(() => {
+          setErrorMessage(null);
+        }, 5000);
+        setNotes(notes.filter((n) => n.id !== id)); //zobrazim len tie poznamky, ktore maju ine id ako ta bludna
+      });
+  };
+
+  const handleNoteChange = (event) => {
+    setNewNote(event.target.value);
+  };
+
+  const notesToShow = showAll ? notes : notes.filter((note) => note.important);
+
+
+  return (
+    <div>
+      <h1>Notes</h1>
+      <Notification message={errorMessage} />
+     {user === null ?
+       loginForm() : 
+       <>
+        <p>{user.name} logged in</p>
+        {notesForm()}
+       </>
+      }
+
+     {notesDisplayed()}
+    </div>
+  );
+};
+
+export default App;
